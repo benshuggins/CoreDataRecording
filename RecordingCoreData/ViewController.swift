82bfd8a8c37bf62cc531c77f182e323515f0c0d5
Ben Huggins <benshuggins@gmail.com>
2019-04-17 23:04:09 -0600

    first commit

---
+++ RecordingCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecordingCoreData
+//
+//  Created by Ben Huggins on 4/17/19.
+//  Copyright © 2019 User. All rights reserved.
+// This is the first commit 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
